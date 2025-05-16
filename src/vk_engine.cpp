3f6d6205e6c689c98f6fdda6b6267cc0fb188c3f
jiaojn <jianingshow@163.com>
2025-05-16 13:59:02 +0800

    change blend mode

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -599,7 +599,9 @@ void VulkanEngine::init_mesh_pipeline()
 	pipelineBuilder.set_polygon_mode(VK_POLYGON_MODE_FILL);
 	pipelineBuilder.set_cull_mode(VK_CULL_MODE_NONE, VK_FRONT_FACE_CLOCKWISE);
 	pipelineBuilder.set_mulitSampling_none();
-	pipelineBuilder.disable_blending();
+	//pipelineBuilder.disable_blending();
+    //pipelineBuilder.enable_bleinding_alphaBlend();
+    pipelineBuilder.enable_blending_additive();
 	pipelineBuilder.enable_depthTest(true, VK_COMPARE_OP_GREATER_OR_EQUAL);
 
 	pipelineBuilder.set_color_attachment_format(_drawImage.imageFormat);
--- src/vk_pipelines.h
+++ src/vk_pipelines.h
@@ -29,6 +29,8 @@ namespace vkutil {
 		void set_cull_mode(VkCullModeFlags cullMode, VkFrontFace frontFace);
 		void set_mulitSampling_none();
 		void disable_blending();
+		void enable_blending_additive();
+		void enable_bleinding_alphaBlend();
 		void set_color_attachment_format(VkFormat format);
 		void set_depth_format(VkFormat format);
 		void disable_depthTest();
